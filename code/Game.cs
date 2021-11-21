11e571bec82e541a2399ab77389d6b0145220097
Drew Britt <drewbritt02@gmail.com>
2021-11-21 03:20:39 -0600

    Basic WordList init (loads local wordlist.txt)

--- code/Game.cs
+++ code/Game.cs
@@ -17,6 +17,7 @@ namespace Sketch
 			if(IsServer)
 			{
 				//Hud = new Hud();
+				Words.InitWordList();
 			}
 		}
 		public override void ClientJoined( Client cl )
---
+++ code/Util/Words.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Sandbox;
+using System.Linq;
+
+namespace Sketch
+{
+	/// <summary>
+	/// Handles random words for the players to draw.
+	/// </summary>
+	public static class Words
+	{
+		public static List<string> WordList;
+		public static string ListPath = "Util/wordlist.txt";
+
+		public static void InitWordList()
+		{
+			if(FileSystem.Mounted.FileExists(ListPath))
+			{
+				WordList = FileSystem.Mounted.ReadAllText( ListPath ).Split( '\n' ).ToList();
+				return;
+			}
+
+			Log.Error( "Sketch: WordList not loaded." );
+		}
+	}
+}
