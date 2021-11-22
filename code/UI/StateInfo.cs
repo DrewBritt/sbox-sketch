71e8a76a2593d72b764eeb694cf461752d290f19
Drew Britt <drewbritt02@gmail.com>
2021-11-22 00:25:21 -0600

    More UI work

---
+++ code/UI/StateInfo.cs
@@ -0,0 +1,30 @@
+﻿using Sandbox.UI;
+using Sandbox;
+using Sandbox.UI.Construct;
+
+namespace Sketch
+{
+	public partial class StateInfo : Panel
+	{
+		public Panel Container;
+		public Label StateName, StateTime;
+
+		public StateInfo()
+		{
+			StyleSheet.Load( "/ui/StateInfo.scss" );
+
+			Container = Add.Panel( "container" );
+			StateName = Container.Add.Label( "State", "statename" );
+			StateTime = Container.Add.Label( "00:00", "statetime" );
+		}
+
+		public override void Tick()
+		{
+			var game = Game.Current;
+			if ( game == null ) return;
+
+			StateName.Text = game.CurrentStateName;
+			StateTime.Text = game.CurrentStateTime;
+		}
+	}
+}
