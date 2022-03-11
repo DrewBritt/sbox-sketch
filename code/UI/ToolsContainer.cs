7dc2fff3c1f164cd4ca53ad8e26f11083d5fe181
Drew Britt <drewbritt02@gmail.com>
2022-03-10 19:08:05 -0600

    Rudimentary drawing tools box

---
+++ code/UI/ToolsContainer.cs
@@ -0,0 +1,17 @@
+﻿using Sandbox.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sketch
+{
+	public partial class ToolsContainer : Panel
+	{
+		public ToolsContainer()
+		{
+			StyleSheet.Load( "ui/ToolsContainer.scss" );
+		}
+	}
+}
