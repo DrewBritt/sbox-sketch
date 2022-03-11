24b4335506b3b325327bbe7a374c52a05a56f255
Drew Britt <drewbritt02@gmail.com>
2022-03-10 19:21:18 -0600

    Update Game to use Sandbox.Game instead of GameBase

--- code/Game.cs
+++ code/Game.cs
@@ -5,17 +5,14 @@ using System.Linq;
 
 namespace Sketch
 {
-	[Library( "Sketch" ), Hammer.Skip]
-	public partial class Game : GameBase
+	public partial class Game :	Sandbox.Game
 	{
-		public static Game Current { get; protected set; }
+		public static new Game Current => Sandbox.Game.Current as Game;
+
 		public HUD Hud { get; set; }
 
 		public Game()
-		{
-			Transmit = TransmitType.Always;
-			Current = this;
-			
+		{	
 			if (IsServer)
 			{
 				Words.InitWordList();
@@ -31,16 +28,6 @@ namespace Sketch
 		public override void ClientDisconnect( Client cl, NetworkDisconnectionReason reason )
 		{
 			ChatBox.AddInformation( To.Everyone, $"{cl.Name} has left ({reason})" );
-		}
-
-		public override void PostLevelLoaded() { }
-
-		public override void Shutdown()
-		{
-			if (Current == this)
-			{
-				Current = null;
-			}
 		}
 
 		protected override void OnDestroy()
@@ -68,36 +55,11 @@ namespace Sketch
 			return camSetup;
 		}
 
-		/// <summary>
-		/// Called after the camera setup logic has run. Allow the gamemode to 
-		/// do stuff to the camera, or using the camera. Such as positioning entities 
-		/// relative to it, like viewmodels etc.
-		/// </summary>
-		public override void PostCameraSetup( ref CameraSetup camSetup )
-		{
-			if ( Local.Pawn != null )
-			{
-				// VR anchor default is at the pawn's location
-				VR.Anchor = Local.Pawn.Transform;
-
-				Local.Pawn.PostCameraSetup( ref camSetup );
-			}
-
-			// Position any viewmodels
-			BaseViewModel.UpdateAllPostCamera( ref camSetup );
-			CameraModifier.Apply( ref camSetup );
-		}
-
 		public override bool CanHearPlayerVoice( Client source, Client dest )
 		{
 			Host.AssertServer();
 
 			return true;
-		}
-
-		public override void OnVoicePlayed( long playerId, float level )
-		{
-			VoiceList.Current?.OnVoicePlayed( playerId, level );
 		}
 	}
 }
