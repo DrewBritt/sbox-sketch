69a1c767c549791a571fe088f23e52a7e56ed222
Drew Britt <drewbritt02@gmail.com>
2022-03-29 02:08:57 -0500

    Refactor StateInfo to use Bind/BindClass

--- code/UI/StateInfo.cs
+++ code/UI/StateInfo.cs
@@ -17,32 +17,16 @@ namespace Sketch
             StyleSheet.Load("/ui/StateInfo.scss");
 
             StateContainer = Add.Panel("statecontainer");
+
             StateName = StateContainer.Add.Label("State", "statename");
+            StateName.Bind("text", () => Game.Current.CurrentStateName.ToUpper());
+
             StateTime = StateContainer.Add.Label("00:00", "statetime");
+            StateTime.Bind("text", () => Game.Current.CurrentStateTime);
 
             WordContainer = Add.Panel("wordcontainer");
             Letters = WordContainer.Add.Label("");
-        }
-
-        public override void Tick()
-        {
-            var game = Game.Current;
-            if(game == null) return;
-
-            StateName.Text = game.CurrentStateName.ToUpper();
-            StateTime.Text = game.CurrentStateTime;
-
-            //Hide container if no letters to display
-            if(Letters.Text == "")
-            {
-                if(!WordContainer.HasClass("empty"))
-                    WordContainer.SetClass("empty", true);
-                return;
-            }
-
-            //Otherwise display
-            if(WordContainer.HasClass("empty"))
-                WordContainer.RemoveClass("empty");
+            WordContainer.BindClass("empty", () => Letters.Text == "");
         }
     }
 }
