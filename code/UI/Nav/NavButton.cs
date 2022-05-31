611a37d9c8e2ec6f8f2ebf2365cc31c9e413abad
Drew Britt <drewbritt02@gmail.com>
2022-05-30 20:43:11 -0500

    Tab navigation system (thanks Crayz <3)

---
+++ code/UI/Nav/NavButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Sandbox;
+using Sandbox.UI;
+
+namespace Sketch;
+
+[Library("navbutton")]
+public class NavButton : Button
+{
+    NavPanel Nav;
+    public string HRef { get; set; }
+
+    public NavButton()
+    {
+        Nav = Ancestors.OfType<NavPanel>().FirstOrDefault();
+        BindClass("active", () => Nav.ContentURL == HRef);
+    }
+
+    public override void SetProperty(string name, string value)
+    {
+        base.SetProperty(name, value);
+
+        if(name == "href")
+        {
+            HRef = value;
+        }
+    }
+
+    protected override void OnMouseDown(MousePanelEvent e)
+    {
+        if(e.Button == "mouseleft")
+        {
+            CreateEvent("navigate", HRef);
+            e.StopPropagation();
+        }
+    }
+}
---
+++ code/UI/Nav/NavPanel.cs
@@ -0,0 +1,34 @@
+﻿using Sandbox.UI;
+
+namespace Sketch;
+
+public partial class NavPanel : Panel
+{
+    public Panel NavContent { get; set; }
+    public string ContentURL { get; set; }
+
+    public void Navigate(string url)
+    {
+        var attribute = NavTargetAttribute.Get(url);
+        if(attribute is null)
+        {
+            Log.Error($"Sketch: NavTarget not found: {url}");
+            return;
+        }
+
+        var panel = TypeLibrary.Create<Panel>(attribute.TargetType);
+        NavContent = panel;
+        ContentURL = url;
+    }
+
+    /// <summary>
+    /// Navigates to the given URL
+    /// </summary>
+    /// <param name="url">URL of content to load</param>
+    [PanelEvent]
+    public bool NavigateEvent(string url)
+    {
+        Navigate(url);
+        return false;
+    }
+}
