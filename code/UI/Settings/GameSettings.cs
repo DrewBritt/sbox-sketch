9e85b7a360604f4d64d28601bec7143824b2a279
Drew Britt <drewbritt02@gmail.com>
2022-05-31 14:40:25 -0500

    Move GameSettings out of Settings menu

---
+++ code/UI/Settings/GameSettings.cs
@@ -0,0 +1,29 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+namespace Sketch;
+
+[UseTemplate]
+[NavTarget("settings/gamesettings")]
+public partial class GameSettings : Panel
+{
+    public int PlayTime { get; set; }
+    public int MaxRounds { get; set; }
+    public int SelectTime { get; set; }
+    public int WordPoolSize { get; set; }
+
+    public GameSettings()
+    {
+        PlayTime = Game.Current.PlayTime;
+        MaxRounds = Game.Current.MaxRounds;
+        SelectTime = Game.Current.SelectWordTime;
+        WordPoolSize = Game.Current.WordPoolSize;
+    }
+
+    public void SetPlayTime() => ConsoleSystem.Run($"sketch_playtime {PlayTime}");
+    public void SetMaxRounds() => ConsoleSystem.Run($"sketch_maxrounds {MaxRounds}");
+    public void SetSelectTime() => ConsoleSystem.Run($"sketch_selectwordtime {SelectTime}");
+    public void SetWordPoolSize() => ConsoleSystem.Run($"sketch_wordpoolsize {WordPoolSize}");
+    public void ResetGame() => ConsoleSystem.Run($"sketch_resetgame");
+    public void SkipWord() => ConsoleSystem.Run($"sketch_skipword");
+}
--- code/UI/Settings/Settings.cs
+++ code/UI/Settings/Settings.cs
@@ -7,27 +7,12 @@ namespace Sketch;
 /// Contains input fields and buttons to let admins control the game.
 /// </summary>
 [UseTemplate]
-public partial class Settings : Panel
+public partial class Settings : NavPanel
 {
-    public int PlayTime { get; set; }
-    public int MaxRounds { get; set; }
-    public int SelectTime { get; set; }
-    public int WordPoolSize { get; set; }
-
     public Settings()
     {
-        PlayTime = Game.Current.PlayTime;
-        MaxRounds = Game.Current.MaxRounds;
-        SelectTime = Game.Current.SelectWordTime;
-        WordPoolSize = Game.Current.WordPoolSize;
+        Navigate("settings/gamesettings");
     }
-
-    public void SetPlayTime() => ConsoleSystem.Run($"sketch_playtime {PlayTime}");
-    public void SetMaxRounds() => ConsoleSystem.Run($"sketch_maxrounds {MaxRounds}");
-    public void SetSelectTime() => ConsoleSystem.Run($"sketch_selectwordtime {SelectTime}");
-    public void SetWordPoolSize() => ConsoleSystem.Run($"sketch_wordpoolsize {WordPoolSize}");
-    public void ResetGame() => ConsoleSystem.Run($"sketch_resetgame");
-    public void SkipWord() => ConsoleSystem.Run($"sketch_skipword");
 
     [Event.BuildInput]
     public void BuildInput(InputBuilder b)
